0b6101650ad6e333221b6f019bd352939f2b48ca
Avabucks <avabuckssociety@gmail.com>
2025-12-12 17:11:21 +0100

    deploy

--- src/app/api/get_places/route.ts
+++ src/app/api/get_places/route.ts
@@ -1,3 +1,18 @@
+"use server"
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  if (request.headers.get('x-cron-key') !== process.env.CRON_KEY) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    return NextResponse.json("DONE");
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
 // TODOs:
 // - script api che return JSON con key_segreta
 
